5431e4c7df163b295ce2e5100f24236f47a24dab
dev-002 <devansh.gupta985@gmail.com>
2023-05-09 22:37:30 +0530

    Default Document for Approve Model Set

--- models/Approve.js
+++ models/Approve.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const approveSchema = new Schema({
-    accepted:[
-        {type: Schema.Types.ObjectId, ref: 'appointment'}
+    accepted: [
+        { type: Schema.Types.ObjectId, ref: 'appointment' }
     ],
     rejected: [
-        {type: Schema.Types.ObjectId, ref: 'appointment'}
+        { type: Schema.Types.ObjectId, ref: 'appointment' }
     ]
-});
+},
+    {
+        accepted: [],
+        rejected: []
+    });
 
 module.exports = mongoose.model('approve', approveSchema);
