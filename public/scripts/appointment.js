56339ff611bbb4ddd1213f910dfc14d04ecd2f2d
dev-002 <devansh.gupta985@gmail.com>
2023-05-11 14:40:33 +0530

    Time Limit and Time Step of 20 Min set

---
+++ public/scripts/appointment.js
@@ -0,0 +1,14 @@
+// function convertTo12HourFormat(time) {
+//     const [hours, minutes] = time.split(':');
+//     let period = 'AM';
+
+//     if (hours >= 12) {
+//         period = 'PM';
+//     }
+
+//     if (hours > 12) {
+//         hours -= 12;
+//     }
+
+//     return `${hours}:${minutes} ${period}`;
+// };
